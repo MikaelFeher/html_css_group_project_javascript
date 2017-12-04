5c929c1f7dc614d76fa68d275ee4b3f3f76fe139
MikaelFeher <mikael.feher@gmail.com>
2017-12-04 09:54:44 +0100

    Adds jquery to the products page Adds jquery rendering of products from the products.js file

---
+++ products.js
@@ -0,0 +1,31 @@
+var html = [
+    {
+        name: 'div',
+        price: 10,
+        info: 'blaha blaha blaha'
+    },
+    {
+        name: 'p',
+        price: 9.99,
+        info: 'blaha blaha blaha'
+    },
+    {
+        name: 'img',
+        price: 22,
+        info: 'blaha blaha blaha'
+    }
+]
+
+var css = [
+    {
+        name: 'font-weight',
+        price: 50,
+		info: 'blaha blaha blaha'
+
+    },
+	{
+        name: 'color',
+        price: 35,
+        info: 'blaha blaha blaha'
+    }
+]
