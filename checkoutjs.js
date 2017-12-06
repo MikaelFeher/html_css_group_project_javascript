4dde6d32806b5d37f39f75f25b521a6cceb8586d
Max Barnell <max.barnell@gmail.com>
2017-12-06 12:05:56 +0100

    something

--- checkoutjs.js
+++ checkoutjs.js
@@ -129,8 +129,11 @@ function getItems() {
 
     $(".reset-button").click(function() {
         $('.empty').show();
-        if($(".all_items").parent().append === "<div class=\"all_items\">")
-        $(".all_items").parent().append("<div class=\"all_items\">");
+
+        if($(".all_items").parent().append === "<div class=\"all_items\">") {
+            $(".all_items").parent().append("<div class=\"all_items\">");
+        }
+
         $('table').html('<tr class="check">\n' +
             '        <th>Namn</th>\n' +
             '        <th>Antal</th>\n' +
